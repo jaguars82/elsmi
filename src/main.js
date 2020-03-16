515297d8319210031512362f1e6c7c0d527cc938
Egor Sechin <jaguars82@yandex.ru>
2020-03-16 15:25:14 +0300

    initial firebase configuration

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Config from './conf'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import Vue2Filters from 'vue2-filters'
 import LangFlag from 'vue-lang-code-flags'
 import UserBadge from './components/User/UserBadge.vue'
+import * as fb from 'firebase'
 
 Vue.config.productionTip = false
 
@@ -18,5 +20,9 @@ new Vue({
   vuetify,
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    fb.initializeApp(Config.firebase)
+    fb.analytics()
+  } 
 }).$mount('#app')
